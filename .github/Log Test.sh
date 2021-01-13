078c29b9d968059b8cc2d9a556619750069763fd
Hatzen <kaihartz@gmx.net>
2021-01-13 17:55:12 +0100

    Update Log Test.sh

--- .github/Log Test.sh
+++ .github/Log Test.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# old Task:
+#./gradlew connectedCheck
+#      - name: Output Crash
+#        run: cat app/build/reports/androidTests/connected/flavors/debugAndroidTest/index.html
+
+# News Task from: https://github.com/kiwix/kiwix-android/blob/develop/contrib/instrumentation_nightly.sh
+
 adb logcat -c
 adb logcat *:E -v color &
 if ./gradlew connectedDebugAndroidTest; then
