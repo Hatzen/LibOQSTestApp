c087448038f8dca81a4042ddcbbf3ad7e4e75c5d
Hatzen <kaihartz@gmx.net>
2021-04-02 01:03:05 +0200

    ~ Connected modules and display supported ciphers

--- app/src/main/java/com/example/liboqstestapp/MainActivity.java
+++ app/src/main/java/com/example/liboqstestapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.liboqstestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.liboqs.KEMs;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupView();
     }
+
+    private void setupView() {
+        Spinner dropdown = findViewById(R.id.supportedAlgorithms);
+
+        String[] items = KEMs.get_enabled_KEMs().toArray(new String[0]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+    }
+
+
 }
