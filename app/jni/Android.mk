f393569fe819610a06384c553ed20f83d2f673e7
Hatzen <kaihartz@gmx.net>
2021-04-01 18:00:01 +0200

    ~ currently failing as wrong libs are detected and leading to be not found (desktop emulator starts running but crashes, yota3 doesnt find it probably as it is arm7?)

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 
 # Prebuild dirs.
 include $(CLEAR_VARS)
-LOCAL_MODULE := oqs-prebuilt
+LOCAL_MODULE := oqs
 LOCAL_SRC_FILES := jniLibs/$(TARGET_ARCH_ABI)/liboqs.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_SHARED_LIBRARY)
@@ -12,8 +12,10 @@ include $(PREBUILT_SHARED_LIBRARY)
 # Create jni wrapper.
 include $(CLEAR_VARS)
 LOCAL_MODULE     := oqs-jni
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/jni/
+# LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_CFLAGS     += -Wall
 LOCAL_SRC_FILES := $(LOCAL_PATH)/jni/handle.c $(LOCAL_PATH)/jni/KEMs.c  $(LOCAL_PATH)/jni/KeyEncapsulation.c  $(LOCAL_PATH)/jni/Rand.c  $(LOCAL_PATH)/jni/Signature.c  $(LOCAL_PATH)/jni/Sigs.c
+LOCAL_LDLIBS    := -llog -landroid
+LOCAL_SHARED_LIBRARIES := oqs
 include $(BUILD_SHARED_LIBRARY)
 
